23b63234ec7abed25b9f49cc62a66323a0be14c4
reyad-hasan <akmkamrulhasanreyad@gmail.com>
2024-09-20 11:45:31 +0600

    completed 2 task

---
+++ task.js
@@ -0,0 +1,47 @@
+// TASK 1 COLORS NAMES REVERS
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// const revColors = [];
+
+// Regular for loop
+// for (let i = 0; i < colors.length; i++) {
+//     const rev = colors[i];
+//     revColors.unshift(rev);
+// }
+
+// for of loop
+// for(color of colors){
+//     revColors.unshift(color);
+// }
+
+// Regular for loop revers
+// for(let i = colors.length-1; i>=0;i--){
+//     const rev = colors[i];
+//     revColors.push(rev);
+// }
+
+// console.log(revColors);
+
+
+
+// TASK 2 FIND EVEN NUMBER AND PUSH NEW ARRAY
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// const evenNumbers = [];
+
+// for of loop
+// for (number of numbers) {
+//     if (number % 2 === 0) {
+//         evenNumbers.push(number);
+//     }
+// }
+// console.log(evenNumbers)
+
+// Regular for loop
+// for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i];
+//     if (number % 2 === 0) {
+//         evenNumbers.push(number)
+//     }
+// }
+// console.log(evenNumbers)
+
+const numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
