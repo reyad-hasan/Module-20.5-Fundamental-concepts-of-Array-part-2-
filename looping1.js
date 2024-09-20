6faaaead6fe10cc602d5735140d875a1a4c31249
reyad-hasan <akmkamrulhasanreyad@gmail.com>
2024-09-20 11:21:04 +0600

    p

---
+++ looping1.js
@@ -0,0 +1,69 @@
+// const friends = ["Ridwan", "Rone", "Rifat", "Tamim"];
+
+// for (friend of friends) {
+//   // console.log(friend);
+// }
+
+// for (let i = 0; i < friends.length; i++) {
+//   // console.log(i);
+//   const name = friends[i];
+//   console.log(i, name);
+// }
+
+// const numbers = [1, 4, 5, 7, 8];
+
+// // for (num of numbers) {
+// //   console.log(num);
+// // }
+
+// for (let n = 0; n < numbers.length; n++) {
+//   // console.log(n);
+//   const num = numbers[n];
+//   console.log(num);
+// }
+
+// const numbers = [1, 4, 5, 7, 88];
+
+// let n = 0;
+// while (n < numbers.length) {
+//   console.log(n);
+//   const num = numbers[n];
+//   console.log(num);
+//   n++;
+// }
+
+// Revers
+// const numbers = [1, 4, 5, 7, 88];
+// const reversed = numbers.reverse();
+// console.log(reversed);
+
+// const numbers = [1, 4, 5, 7, 88];
+// const rev_numbers = [];
+// for (num of numbers) {
+//   rev_numbers.unshift(num);
+// }
+// console.log(rev_numbers);
+
+// Regular for loop using revers
+// const numbers = [1, 4, 5, 77, 88];
+// const rev_regular = [];
+// for (let i = numbers.length - 1; i >= 0; i--) {
+//   const rev = numbers[i];
+//   rev_regular.push(rev);
+// }
+// console.log(rev_regular);
+
+// sort method
+// const numbers = [1, 3, 14, 5, 7, 88];
+// const numbers = ['rakib', 'nokib', 'sakib', 'akib', 'Pakib', 'anis', 'Anis'];
+// const sortPersons = numbers.sort();
+// console.log(sortPersons);
+
+// Ascending --> smaller to larger
+// Descending --> larger to smaller
+
+// const numberAsc = [...numbers].sort(function (a, b) { return a - b });
+// const numberDsc = [...numbers].sort(function (a, b) { return b - a });
+
+// console.log(numberAsc);
+// console.log(numberDsc);
